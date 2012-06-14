680e0b569469cd7a7005eb78ffdffe2d28edb992
Calpo <calpo22@gmail.com>
2012-06-14 11:02:25 +0900

    1.5 fin

--- baby.hs
+++ baby.hs
@@ -8,3 +8,9 @@ doubleSmallNumber x = if x > 100
 
 --doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 doubleSmallNumber' x = doubleSmallNumber x + 1
+
+boomBang xs = [ if x < 10 then "Boom!" else "Ahh!" | x <- xs, odd x, x < 20]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
