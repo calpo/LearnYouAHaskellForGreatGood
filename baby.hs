5d609a4c4f7a3e984ccb2a52aaa7e691590fa9c7
Calpo <calpo22@gmail.com>
2012-06-14 16:10:45 +0900

    2.1 fin 関数の型宣言が本の通りにいかない

--- baby.hs
+++ baby.hs
@@ -13,8 +13,12 @@ boomBang xs = [ if x < 10 then "Boom!" else "Ahh!" | x <- xs, odd x, x < 20]
 
 length' xs = sum [1 | _ <- xs]
 
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 --triples' x = [ (a, b, c) | c <- [1..x], a <- [1..c], b <- [1..a] ]
 --triples' x = [ (a, b, c) | c <- [1..x], a <- [1..x], b <- [1..x], c < a + b, a <= c, b <= c, a <= b ]
 triples' x = [ (a, b, c) | c <- [1..x], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2 ]
+
+--addThree Int -> Int -> Int -> Int
+addThree x y z = x + y + z
