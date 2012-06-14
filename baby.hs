97ba4e8af3214cabb5b83f9881a1cef15e455cdc
Calpo <calpo22@gmail.com>
2012-06-14 09:50:20 +0900

    1.2 fin

--- baby.hs
+++ baby.hs
@@ -1,1 +1,10 @@
 doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+						then x
+						else x*2
+
+--doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+doubleSmallNumber' x = doubleSmallNumber x + 1
