a73a7ee8872cbb361d3bf6af80fc88145619f58d
Calpo <calpo22@gmail.com>
2012-06-15 10:01:20 +0900

    2.4 p32

--- baby.hs
+++ baby.hs
@@ -1,24 +1,41 @@
+--doubleMe :: Num a => a -> a
+doubleMe :: Int -> Int
 doubleMe x = x + x
 
+--doubleUs :: Num a => a -> a -> a
+doubleUs :: Int -> Int -> Int
 doubleUs x y = doubleMe x + doubleMe y
 
+--doubleSmallNumber :: Num a => a -> a
+doubleSmallNumber :: Int -> Int
 doubleSmallNumber x = if x > 100
 						then x
 						else x*2
 
---doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+doubleSmallNumber' :: Int -> Int
 doubleSmallNumber' x = doubleSmallNumber x + 1
 
+boomBang :: [Int] -> [[Char]]
 boomBang xs = [ if x < 10 then "Boom!" else "Ahh!" | x <- xs, odd x, x < 20]
 
+length' :: [t] -> Int
 length' xs = sum [1 | _ <- xs]
 
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
---triples' x = [ (a, b, c) | c <- [1..x], a <- [1..c], b <- [1..a] ]
---triples' x = [ (a, b, c) | c <- [1..x], a <- [1..x], b <- [1..x], c < a + b, a <= c, b <= c, a <= b ]
+triples' :: Int -> [(Int, Int, Int)]
 triples' x = [ (a, b, c) | c <- [1..x], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2 ]
 
---addThree Int -> Int -> Int -> Int
+--addThree :: Num a => a -> a-> a-> a
+addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
