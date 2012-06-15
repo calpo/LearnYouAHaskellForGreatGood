88dc061b68f3c69bb528cfc4f052750eeee943f8
Calpo <calpo22@gmail.com>
2012-06-15 18:33:41 +0900

    3.1 p38

--- baby.hs
+++ baby.hs
@@ -32,10 +32,57 @@ addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
 factorial :: Integer -> Integer
-factorial n = product [1..n]
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+
+fizzbuzz' :: Int -> [Char]
+fizzbuzz' 2 = "fizz"
+fizzbuzz' 3 = "buzz"
+fizzbuzz' 4 = "fizz"
+fizzbuzz' 6 = "fizzbuzz"
+fizzbuzz' x = show x
+
+-- fizz buzz challenge
+fizz :: Int -> String
+fizz x = if x `mod` 3 == 0 then "fizz" else ""
+
+buzz :: Int -> String
+buzz x = if x `mod` 4 == 0 then "buzz" else ""
+
+fandb :: Int -> String
+fandb x = fizz x ++ buzz x
+
+fandb' :: Int -> String
+fandb' x = if fandb x == "" then show x else fandb x
+
+fizzbuzz :: Int -> [String]
+fizzbuzz x = [ fandb' a | a <- [1..x] ]
+
+fibonatch 0 = 1
+fibonatch 1 = 1
+fibonatch x = fibonatch (x - 2) + fibonatch (x - 1)
+fibonatchList xs = [ fibonatch x | x <- [1..xs] ]
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+--charName x = [x]
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+--addVectors a b = (fst a + fst b, snd a + snd b)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, x, _) = x
+
+third :: (a, b, c) -> c
+third (_, _, x) = x
