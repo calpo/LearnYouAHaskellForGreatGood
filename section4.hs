b49895e5bb8fec1cd7ec26c62b74b8617c0cab1a
Calpo <calpo22@gmail.com>
2012-06-20 08:46:52 +0900

    4.2 fin

--- section4.hs
+++ section4.hs
@@ -19,3 +19,21 @@ take' n _
     | n <= 0            = []
 take' _ [] = []
 take' n (x:xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = elem' a xs
