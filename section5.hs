e73cb6e285b94d021cba9cc67e07fb334684d72a
Calpo <calpo22@gmail.com>
2012-06-20 11:18:57 +0900

    5.2 fin

---
+++ section5.hs
@@ -0,0 +1,28 @@
+{-# OPTIONS -Wall -Werror #-}
+
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: Floating a => a -> a
+divideByTen = (/10)
+
+isUpperAlpanum :: Char -> Bool
+isUpperAlpanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f x y = f y x
