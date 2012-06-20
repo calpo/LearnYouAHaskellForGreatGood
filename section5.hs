1eaf3a2ed19e586666dd2f8a34c53c68f9692ddb
Calpo <calpo22@gmail.com>
2012-06-20 22:55:27 +0900

    5.3 fin

--- section5.hs
+++ section5.hs
@@ -26,3 +26,38 @@ flip' f = g
 
 flip'' :: (a -> b -> c) -> b -> a -> c
 flip'' f x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = [f x | x <- xs]
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' _ [] = []
+map'' f (x:xs) = f x : map'' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = filter (<=x) xs
+        larger = filter (>x) xs
+    in  quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+largerstDivisible :: Integer
+largerstDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n  = n : chain (n * 3 + 1)
+    | otherwise = [1]
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
