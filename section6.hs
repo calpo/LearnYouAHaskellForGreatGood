1020794bf4f3802a53e0cd49d8c0d1e0836b2e35
Calpo <calpo22@gmail.com>
2012-06-22 19:27:13 +0900

    6.2 p96

--- section6.hs
+++ section6.hs
@@ -2,6 +2,7 @@
 
 --import qualified Data.List as L
 import Data.List
+import Data.Char
 
 numUniques :: Eq a => [a] -> Int
 numUniques = length . nub
@@ -12,3 +13,11 @@ wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
 
 isIn :: Eq a => [a] -> [a] -> Bool
 needle `isIn` haystack = any (isPrefixOf needle) (tails haystack)
+
+encode :: Int -> String -> String
+--encode offset msg = map (\c -> chr ((ord c) + offset)) msg
+--encode offset msg = map (\c -> chr $ ord c + offset) msg
+encode offset = map $ chr . (+offset) . ord
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
